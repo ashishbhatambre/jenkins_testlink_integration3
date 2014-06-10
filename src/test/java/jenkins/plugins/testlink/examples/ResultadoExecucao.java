e4a236d0f6a736ac2d205ae1e1123680eab6cc8b
ashishbhatambre <iitm1710@gmail..com>
2014-06-10 14:23:04 +0530

    4th commit

---
+++ src/test/java/jenkins/plugins/testlink/examples/ResultadoExecucao.java
@@ -0,0 +1,17 @@
+package jenkins.plugins.testlink.examples;
+import testlink.api.java.client.TestLinkAPIClient;
+import testlink.api.java.client.TestLinkAPIException;
+
+public class ResultadoExecucao implements IConstantes {
+public static void reportTestCaseResult1(String projetoTeste, String planoTeste, String casoTeste, String nomeBuild, String nota, String resultado) throws TestLinkAPIException {
+try {
+	TestLinkAPIClient testlinkAPIClient = new TestLinkAPIClient(DEVKEY, URL);
+testlinkAPIClient.reportTestCaseResult(projetoTeste, planoTeste, casoTeste, nomeBuild, nota, resultado);
+}
+catch(Exception e){
+	e.printStackTrace();
+}
+}
+
+}
+
